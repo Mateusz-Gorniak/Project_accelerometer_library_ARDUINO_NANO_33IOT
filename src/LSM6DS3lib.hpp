a065739007fce4ec028677088d855e5147f957b7
Mateusz <mateusz.gorniak@student.wat.edu.pl>
2021-05-24 23:18:26 +0200

    Project organisation

---
+++ src/LSM6DS3lib.hpp
@@ -0,0 +1,7 @@
+#ifndef _LSM6DS3LIB_H_
+
+//Importowanie bibliotek
+#include "Arduino.h"
+#include "Wire.h"
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
-#include <Arduino.h>
+#include <LSM6DS3lib.hpp>
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
 }
